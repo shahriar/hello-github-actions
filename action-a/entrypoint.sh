da612a3fabdaf07e0f9ebbfb62632f7b43eaa3a3
Md. Shahriar Kabir <shahriar040@gmail.com>
2020-07-23 16:00:43 +0600

    ssh-add command fix

--- action-a/entrypoint.sh
+++ action-a/entrypoint.sh
@@ -6,7 +6,8 @@ apt-get install -qq git
 # Setup SSH deploy keys
 apt-get install -qq openssh-client
 eval $(ssh-agent -s)
-ssh-add - <<< "${SSH_PRIVATE_KEY}"
+echo "$SSH_PRIVATE_KEY"
+echo "$SSH_PRIVATE_KEY" | ssh-add -
 mkdir -p ~/.ssh
 echo -e "Host *\n\tStrictHostKeyChecking no\n\n" > ~/.ssh/config
 
