1425455a6e2a9358f81f064ed7d4daaad1a6c793
Md. Shahriar Kabir <shahriar040@gmail.com>
2020-07-23 16:59:13 +0600

    ssh-add command fix

--- action-a/entrypoint.sh
+++ action-a/entrypoint.sh
@@ -8,7 +8,7 @@ apt-get install -qq openssh-client
 eval $(ssh-agent -s)
 echo "$SSH_PRIVATE_KEY" | ssh-add -
 mkdir -p ~/.ssh
-echo -e "Host *\n\tStrictHostKeyChecking no\n\n" > ~/.ssh/config
+echo -e "Host *\n\tStrictHostKeyChecking no\n\tAddKeysToAgent $REMOTE_HOST\n\n" > ~/.ssh/config
 
-ssh -T -$REMOTE_HOST $REMOTE_USER@$REMOTE_HOST "cd $REMOTE_TARGET && touch testing.txt && exit"
+ssh -T $REMOTE_USER@$REMOTE_HOST "cd $REMOTE_TARGET && touch testing.txt && exit"
 echo "Finished deploy"
