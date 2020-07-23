943e7a1e5436cba3477cf46bab6aacdefaeec374
Md. Shahriar Kabir <shahriar040@gmail.com>
2020-07-23 15:14:21 +0600

    add ssh client

--- action-a/entrypoint.sh
+++ action-a/entrypoint.sh
@@ -2,12 +2,13 @@
 
 apt-get update -qq
 apt-get install -qq git
-# Setup SSH deploy keys
-'which ssh-agent || ( apt-get install -qq openssh-client )'
-eval $(ssh-agent -s)
-ssh-add <(echo "$SSH_PRIVATE_KEY")
-mkdir -p ~/.ssh
-echo -e "Host *\n\tStrictHostKeyChecking no\n\n" > ~/.ssh/config
 
-ssh $REMOTE_USER@$REMOTE_HOST "cd $REMOTE_TARGET && touch testing.txt"
+# Setup SSH deploy keys
+apt-get install -qq openssh-client
+eval $(ssh-agent -s)
+# ssh-add <(echo "$SSH_PRIVATE_KEY")
+# mkdir -p ~/.ssh
+# echo -e "Host *\n\tStrictHostKeyChecking no\n\n" > ~/.ssh/config
+
+# ssh $REMOTE_USER@$REMOTE_HOST "cd $REMOTE_TARGET && touch testing.txt"
 sh -c "Finished deploy"
