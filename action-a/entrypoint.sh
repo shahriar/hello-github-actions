0a2bf224aa4be075be59d1a90741ea371960a811
Md. Shahriar Kabir <shahriar040@gmail.com>
2020-07-24 10:48:24 +0600

    entrypoint changed

--- action-a/entrypoint.sh
+++ action-a/entrypoint.sh
@@ -8,7 +8,8 @@ apt-get install -qq openssh-client
 eval $(ssh-agent -s)
 echo "$SSH_PRIVATE_KEY" | ssh-add -
 mkdir -p ~/.ssh
-echo -e "Host *\n\tStrictHostKeyChecking no\n\tStrictHostKeyChecking no\n\n" > ~/.ssh/config
+echo -e "Host *\n\tStrictHostKeyChecking no\n\n" > ~/.ssh/config
+chmod 600 ~/.ssh/id_rsa
 
-ssh -T ubuntu@ec2-13-239-59-103.ap-southeast-2.compute.amazonaws.com "cd $REMOTE_TARGET && touch testing.txt && exit"
+ssh -T ubuntu@13.238.159.198 "cd / && touch testing.txt && exit"
 echo "Finished deploy"
