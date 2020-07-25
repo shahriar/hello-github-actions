a377762905001ed54c6c437c6b159a2ab360fff0
Md. Shahriar Kabir <shahriar040@gmail.com>
2020-07-25 13:55:54 +0600

    mod removed

--- action-a/entrypoint.sh
+++ action-a/entrypoint.sh
@@ -9,7 +9,6 @@ eval $(ssh-agent -s)
 echo "$SSH_PRIVATE_KEY" | ssh-add -
 mkdir -p ~/.ssh
 echo -e "Host *\n\tStrictHostKeyChecking no\n\n" > ~/.ssh/config
-chmod 600 ~/.ssh/id_rsa
 
-ssh -T ubuntu@13.238.159.198 "cd / && touch testing.txt && exit"
+ssh -T ubuntu@13.238.159.198 "cd /home/ubuntu/tura && touch testing.txt && exit"
 echo "Finished deploy"
