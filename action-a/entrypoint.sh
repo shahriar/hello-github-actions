d13db31f2620e7c71e67fef99c3d4ed184c0c293
Md. Shahriar Kabir <shahriar040@gmail.com>
2020-07-25 14:03:15 +0600

    home changed

--- action-a/entrypoint.sh
+++ action-a/entrypoint.sh
@@ -8,7 +8,7 @@ apt-get install -qq openssh-client
 eval $(ssh-agent -s)
 echo "$SSH_PRIVATE_KEY" | ssh-add -
 mkdir -p ~/.ssh
-echo -e "Host *\n\tStrictHostKeyChecking no\n\n" > ~/.ssh/config
+echo -e "Host *\n\tStrictHostKeyChecking no\n\n" > $HOME/.ssh/config
 
 ssh -T ubuntu@13.238.159.198 "cd /home/ubuntu/tura && touch testing.txt && exit"
 echo "Finished deploy"
