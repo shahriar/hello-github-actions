04b44b397d433fd166771bff47e85e9c60abd4ea
Md. Shahriar Kabir <shahriar040@gmail.com>
2020-07-26 17:37:08 +0600

    config change

--- action-a/entrypoint.sh
+++ action-a/entrypoint.sh
@@ -8,9 +8,9 @@ apt-get install -qq openssh-client
 eval $(ssh-agent -s)
 echo $SSH_PRIVATE_KEY | ssh-add -
 mkdir -p ~/.ssh
-echo -e "Host *.compute.amazonaws.com\n\tStrictHostKeyChecking no\n\tUserKnownHostsFile /dev/null\n\n" > ~/.ssh/config
+echo -e "Host *.compute.amazonaws.com\n\tStrictHostKeyChecking no\n\tUserKnownHostsFile /dev/null\n\RSAAuthentication yes\n\PubkeyAuthentication yes\n\n" > ~/.ssh/config
 cat ~/.ssh/config
 ls ~/.ssh/
 
-ssh -vT -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null ubuntu@13.238.159.198 "cd /home/ubuntu/tura && touch testing.txt && exit"
+ssh -vT ubuntu@13.238.159.198 "cd /home/ubuntu/tura && touch testing.txt && exit"
 echo "Finished deploy"
