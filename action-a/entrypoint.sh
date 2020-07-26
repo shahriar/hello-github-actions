7d2a1d74731bca5c669fa25787aeff49f7ed01c3
Md. Shahriar Kabir <shahriar040@gmail.com>
2020-07-26 20:59:54 +0600

    fixed pem file location to action-a

--- action-a/entrypoint.sh
+++ action-a/entrypoint.sh
@@ -9,7 +9,7 @@ eval $(ssh-agent -s)
 echo $SSH_PRIVATE_KEY | ssh-add -
 mkdir -p ~/.ssh
 echo -e "Host *.compute.amazonaws.com\n\tStrictHostKeyChecking no\n\tUserKnownHostsFile /dev/null\n\RSAAuthentication yes\n\PubkeyAuthentication yes\n\n" > ~/.ssh/config
-chmod 600 ~/.ssh/*
+chmod 400 staging-server.pem
 
-ssh -vT -i ./staging-server.pem ubuntu@13.238.159.198 "cd /home/ubuntu/tura && touch testing.txt && exit"
+ssh -i "staging-server.pem" ubuntu@13.238.159.198 "cd /home/ubuntu/tura && touch testing.txt && exit"
 echo "Finished deploy"
