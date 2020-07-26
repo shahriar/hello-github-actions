9654c53bf20dec8a3862da66e654b4100833a521
Md. Shahriar Kabir <shahriar040@gmail.com>
2020-07-26 17:45:43 +0600

    check chmod

--- action-a/entrypoint.sh
+++ action-a/entrypoint.sh
@@ -9,8 +9,7 @@ eval $(ssh-agent -s)
 echo $SSH_PRIVATE_KEY | ssh-add -
 mkdir -p ~/.ssh
 echo -e "Host *.compute.amazonaws.com\n\tStrictHostKeyChecking no\n\tUserKnownHostsFile /dev/null\n\RSAAuthentication yes\n\PubkeyAuthentication yes\n\n" > ~/.ssh/config
-cat ~/.ssh/config
-ls ~/.ssh/
+chmod 600 ~/.ssh/*
 
 ssh -vT ubuntu@13.238.159.198 "cd /home/ubuntu/tura && touch testing.txt && exit"
 echo "Finished deploy"
