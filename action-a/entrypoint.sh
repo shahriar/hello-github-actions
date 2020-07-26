0fa17d27aeb5e8de92322e1615f074e3e4f66768
Md. Shahriar Kabir <shahriar040@gmail.com>
2020-07-26 21:49:25 +0600

    fixed pem file location to action-a

--- action-a/entrypoint.sh
+++ action-a/entrypoint.sh
@@ -1,15 +1,18 @@
 #!/bin/sh -l
 
-apt-get update -qq
-apt-get install -qq git
+# apt-get update -qq
+# apt-get install -qq git
 
 # Setup SSH deploy keys
-apt-get install -qq openssh-client
-eval $(ssh-agent -s)
-echo $SSH_PRIVATE_KEY | ssh-add -
-mkdir -p ~/.ssh
-echo -e "Host *.compute.amazonaws.com\n\tStrictHostKeyChecking no\n\tUserKnownHostsFile /dev/null\n\RSAAuthentication yes\n\PubkeyAuthentication yes\n\n" > ~/.ssh/config
+# apt-get install -qq openssh-client
+# eval $(ssh-agent -s)
+# echo $SSH_PRIVATE_KEY | ssh-add -
+# mkdir -p ~/.ssh
+# echo -e "Host *.compute.amazonaws.com\n\tStrictHostKeyChecking no\n\tUserKnownHostsFile /dev/null\n\RSAAuthentication yes\n\PubkeyAuthentication yes\n\n" > ~/.ssh/config
 chmod 400 ./action-a/staging-server.pem
 
-ssh -i "action-a/staging-server.pem" ubuntu@13.238.159.198 "cd /home/ubuntu/tura && touch testing.txt && exit"
+
+
+ssh -i ./action-a/staging-server.pem ubuntu@13.238.159.198 "cd /home/ubuntu/tura && touch testing.txt && exit"
 echo "Finished deploy"
+echo $PWD
